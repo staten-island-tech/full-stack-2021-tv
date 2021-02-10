eff4e19f3befac4c865ad3fd949984219bffab39
AlanGeorge337 <56314602+AlanGeorge337@users.noreply.github.com>
2021-02-10 09:52:34 -0500

    ooof starting off

--- basics/src/App.vue
+++ basics/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
       <div id="nav">
-        <router-link to="/Login">Login </router-link>
+        <router-link to="/login">Login </router-link> |
         <router-link to="/">Logo</router-link> |
         <router-link to="/about">Profile </router-link>
       </div>
--- basics/src/views/Home.vue
+++ basics/src/views/Home.vue
@@ -1,38 +1,68 @@
 <template>
-<!-- change home to icon  -->
-  <section class="banner-home">
-    <div class="home"></div>
-    <p id = "tag-drop-down"> drop down tags </p>
-    <p id = "make-blog"> blog </p>
-  </section>
+    <div class="login-box">
+        <form>
+            <h1 class= "login-title">Login</h1>
+            <div class="form-group">
+                <label>Email address</label>
+                <input type="email" class="form-control form-control-lg" />
+            </div>
 
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" class="form-control form-control-lg" />
+            </div>
+            <div class = "login-signup-btn">
+            <label id = "signUp-btn">Make An Account</label>
+            <button type="submit" class="btn btn-dark btn-lg btn-block" id ="login-btn">Login</button>
+            </div>
+        </form>
+    </div>
 </template>
 
-<style lang="scss">
-.banner-home {
-  height: 6rem;
-  margin: auto;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-}
-
-#tag-drop-down {
-  width: 90%;
-  border: 2px solid;
-}
-
-#make-blog {
-    width: 10%;
-    border: 2px solid;
-    border-left: none;
-}
-
-
+<style>
+  .login-box {
+    width: 90vh;
+    margin: auto;
+    border-style: solid;
+    border-color: black;
+    border-width: 2px;
+    padding: 2rem;
+    margin-top: 8%;
+  }
+  
+  .login-title {
+    text-align: center;
+  }
+  .login-signup-btn{
+    display: flex;
+    flex-direction: row;
+  }
+  #login-btn {
+    width: 40%;
+  }
+  #signUp-btn {
+    width: 60%;
+  }
 </style>
 
+
+
+<script src="https://www.gstatic.com/firebasejs/8.2.6/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.2.6/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.2.6/firebase-firestore.js"></script>
+
 <script>
-// @ is an alias to /src
-
-
+  var firebaseConfig = {
+    apiKey: "AIzaSyA59N6G1QldK2tKnDZug9sML6-e5_7MBBA",
+    authDomain: "full-stack-b060c.firebaseapp.com",
+    projectId: "full-stack-b060c",
+    storageBucket: "full-stack-b060c.appspot.com",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  //make auth and firestore references 
+  const auth = firebase.auth();
+  const db = firebase.firestore();
+  db.settings({timestampsInSnapshots: true});
 </script>
