95048b70a89e1da53e79a60bca079a5b14f9cb1d
AlanGeorge337 <56314602+AlanGeorge337@users.noreply.github.com>
2021-03-03 11:08:26 -0500

    proto

---
+++ basics/src/components/Modals-for-Profile.vue
@@ -0,0 +1,22 @@
+<template>
+    <modal name="settings_on_profile_modal">
+        Settings
+    </modal>
+</template>
+
+<script>
+export default {
+    name: 'MyComponent',
+    methods: {
+        show () {
+            this.$modal.show('my-first-modal');
+        },
+        hide () {
+            this.$modal.hide('my-first-modal');
+        }
+    },
+    mount () {
+        this.show()
+    }
+}
+</script>
