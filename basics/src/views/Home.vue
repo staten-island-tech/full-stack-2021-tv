57146c27b597279947b7bbb6cae050a951b52be1
bickyy <vicky.zhou21@sitechhs.com>
2021-03-06 18:07:07 -0500

    added button and drop down

--- basics/src/views/Home.vue
+++ basics/src/views/Home.vue
@@ -4,14 +4,38 @@
       <div class="user-profile"></div>
       <p id="logo-for-feed"></p>
       <p id="banner-profile-empty-space"></p>
+      <p id="button-to-make-blog">
+        <b-button id="show-btn" @click="$bvModal.show('bv-modal-example')"
+          >+</b-button
+        >
+        <b-modal id="bv-modal-example" hide-footer>
+          <div>
+            <h3></h3>
+          </div>
+        </b-modal>
+      </p>
       <p id="settings"></p>
     </section>
 
     <section class="feed">
       <div class="picture">
-        <div class="post-button">...</div>
-        <div class="likes">O likes</div>
+        <b-dropdown
+          variant="outline-dark"
+          class="report-button"
+          size="lg"
+          no-caret
+        >
+          <template #button-content>
+            <span>...</span>
+          </template>
+          <b-dropdown-item href="#">Report</b-dropdown-item>
+        </b-dropdown>
+
+        <div class="likes">
+          <b-icon variant="danger" icon="heart"></b-icon> 1 like
+        </div>
       </div>
+
       <div class="description-comment">
         <div class="description">
           <router-link to="/ProfileOther" class="username"> Name </router-link>
@@ -51,11 +75,33 @@ export default {};
 }
 
 #banner-profile-empty-space {
-  width: 90%;
+  width: 84%;
   border: 2px solid;
   border-left: none;
 }
+//make a blog button
+#button-to-make-blog {
+  width: 6%;
+  border: 2px solid;
+  border-left: none;
+}
+.btn-secondary {
+  background-color: white;
+  color: black;
+}
 
+.btn[data-v-fae5bece] {
+  font-size: 3rem;
+  width: 100%;
+  height: 100%;
+  border-radius: 0rem;
+}
+
+.btn {
+  padding: 0rem;
+}
+
+//
 #settings {
   width: 5%;
   border: 2px solid;
@@ -87,16 +133,16 @@ export default {};
   margin-bottom: 0;
 }
 
-.post-button {
+.report-button {
   float: right;
   width: 4rem;
   height: 4rem;
-  font-size: 2rem;
-  border: 1px solid;
-  text-align: center;
-  border-top: none;
-  border-right: none;
 }
+// css for bootstrap
+.dropdown-menu {
+  min-width: 5rem;
+}
+//
 
 .description-comment {
   margin-left: 1rem;
