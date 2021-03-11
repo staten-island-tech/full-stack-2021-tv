42886b5a32a47ac36aafd1b5127b90b620a287a5
bickyy <vicky.zhou21@sitechhs.com>
2021-03-11 11:45:45 -0500

    Revert "Merge branch 'profile-page' into staging"
    This reverts commit 7f31ee171edc2628c2c0ddf6a12a08c620112370, reversing
    changes made to b9a338083ab1a8540055f405f14f51e105cf3cfc.

--- basics/src/views/Profile.vue
+++ basics/src/views/Profile.vue
@@ -2,28 +2,28 @@
   <div>
     <section class="user-banner-profile">
       <div class="user-profile"></div>
-      <p id="logo-for-feed">Logo</p>
+      <p id="logo-for-feed"></p>
       <p id="banner-profile-empty-space"></p>
-      <p id="settings">Settings</p>
+      <p id="settings"></p>
     </section>
 
     <section class="info-about-user">
       <div class="user-profile-pic/generic-info"></div>
-      <div class="user-profile-pic">Profile Picture</div>
+      <div class="user-profile-pic"></div>
       <div class="generic-info">
         <div id="user-following-followers">
-          <p id="user-following">Following</p>
-          <p id="user-followers">Followers</p>
+          <p id="user-following"></p>
+          <p id="user-followers"></p>
         </div>
-        <p id="user-username-n-bio">Username and Bio</p>
+        <p id="user-username-n-bio"></p>
       </div>
     </section>
 
     <section class="user-banner-posts">
       <div class="user-posts/likes"></div>
-      <p id="user-public-post">Public Posts</p>
-      <p id="user-private-post">Private Posts</p>
-      <p id="user-liked-post">Liked Posts</p>
+      <p id="user-public-post"></p>
+      <p id="user-private-post"></p>
+      <p id="user-liked-post"></p>
     </section>
 
     <section class="user-posts">
@@ -43,25 +43,21 @@
 
       <div class="user-post-column-2">
         <div id="user-actual-posts">
-          Post
-          <div id="post-settings-with-empty-space">
-            <p id="post-settings-empty-space"></p>
-            <p id="post-settings-button">...</p>
-          </div>
-          <div id="post-picture"></div>
-          <div id="post-likes">
-            <b-icon variant="danger" icon="heart"></b-icon> 1 like
-          </div>
+          <p id="post-settings-button"></p>
+          <p id="post-likes"></p>
         </div>
       </div>
     </section>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {};
+</script>
 
 <style lang="scss" scoped>
 
 @import "@/styles/_variables.scss";
 @import "@/styles/Profile.scss";
+
 </style>
