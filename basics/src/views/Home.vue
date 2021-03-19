f5950c9cae56e4fd7b015cbfacbf46ca267452d0
bickyy <57456211+bickyy@users.noreply.github.com>
2021-03-19 09:29:38 -0400

    Revert "Merge branch 'home-page-' into staging"
    This reverts commit c751e34cbc948317d9315b3cc361c958974e7bf7, reversing
    changes made to 4bbad73efbde4889ff8abfe80dd0abebf2e6471c.

--- basics/src/views/Home.vue
+++ basics/src/views/Home.vue
@@ -7,53 +7,12 @@
         <v-select id="mySelect" :options="options"></v-select>
       </div>
       <!-- <v-select id="select-tag-container" :options="options"></v-select> -->
-        <button onclick="document.getElementById('id01').style.display='block'" class="w3-button w3-black">Open Modal</button>
-
-  <div id="id01" class="w3-modal">
-    <div class="w3-modal-content">
-      <div class="w3-container">
-        <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-display-topright">&times;</span>
-          <div class="modal-body">
-            <div class="modal-header"> </div>
-            <div class="blog-picture">
-              <b-form-file
-                accept="image/jpeg, image/png, image/gif"
-              ></b-form-file>
-            </div>
-            <div class="blog-comment">
-                <textarea
-                  placeholder="Caption"
-                  class = "blog-caption"
-                ></textarea>
-            </div>
-
-            <div class="blog-bottom-row">
-              <select class="blog-tag" v-model="blog.tag">
-                <option v-for="tag in tags" v-bind:key="tag">
-                  {{ tag }}
-                </option>
-              </select>
-
-              <select class="blog-toggle-private-public">
-                <option value="1" selected>Public</option>
-                <option value="2">Private</option>
-              </select>
-
-              <div class="blog-post">
-                Post
-              </div>
-            </div>
-          </div>
-      </div>
-    </div>
-  </div>
-
-      <!-- <p id="button-to-make-blog">
+      <p id="button-to-make-blog">
         <b-button id="show-btn" @click="$bvModal.show('making-blog')"
           >+</b-button
-        > -->
-        <!-- <b-modal id="making-blog" size="xl" hide-footer> -->
-          <!-- <div class="modal-body">
+        >
+        <b-modal id="making-blog" size="xl" hide-footer>
+          <div class="modal-body">
             <div class="blog-picture">
               <b-form-file
                 accept="image/jpeg, image/png, image/gif"
@@ -79,7 +38,7 @@
                   {{ tag }}
                 </option>
               </select>
-
+              
               <select class="blog-toggle-private-public">
                 <option value="1" selected>Public</option>
                 <option value="2">Private</option>
@@ -89,9 +48,9 @@
                 Post
               </div>
             </div>
-          </div> -->
-        <!-- </b-modal>
-      </p> -->
+          </div>
+        </b-modal>
+      </p>
 
       <p id="settings"></p>
     </section>
@@ -104,19 +63,11 @@
           </template>
           <b-dropdown-item href="#">Report</b-dropdown-item>
         </b-dropdown>
-      <div class="feed-post">
-        <div class="picture">
-          <b-dropdown variant="none" class="report-button" size="lg" no-caret>
-            <template #button-content>
-              <span>...</span>
-            </template>
-            <b-dropdown-item href="#">Report</b-dropdown-item>
-          </b-dropdown>
 
-          <div class="likes">
-            <b-icon variant="danger" icon="heart"></b-icon> 1 like
-          </div>
+        <div class="likes">
+          <b-icon variant="danger" icon="heart"></b-icon> 1 like
         </div>
+      </div>
 
       <div class="description-comment">
         <div class="description">
@@ -124,14 +75,6 @@
           <p class="caption">caption</p>
         </div>
         <div class="comment-section">
-        <div class="description-comment">
-          <div class="description">
-            <router-link to="/ProfileOther" class="username">
-              Name
-            </router-link>
-            <p class="caption">caption</p>
-          </div>
-          <!-- <div class="comment-section">
           <router-link
             to="/ProfileOther"
             id="comment-username"
@@ -140,7 +83,6 @@
             Name
           </router-link>
           <p class="comment">comment</p>
-        </div> -->
         </div>
       </div>
     </section>
@@ -182,18 +124,19 @@ export default {
 };
 </script>
 
+
 <style lang="scss">
 @import "@/styles/Home.scss";
 </style>
 
 <style>
 .vs--searchable .vs__dropdown-toggle {
-  border: none;
+  border: none ;
 }
 .btn-secondary {
-  color: black  !important;
-  width: 100%;
-  height: 100%;
-  border: 0rem  !important;
+    background-color: white;
+    color: black;
+    border: 0rem;
 }
 </style>
+
