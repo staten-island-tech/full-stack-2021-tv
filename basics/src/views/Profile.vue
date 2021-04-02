a760263264e10ef2ad58d3acf12988bbd19bcc95
bickyy <vicky.zhou21@sitechhs.com>
2021-04-02 17:13:39 -0400

    added button for settings

--- basics/src/views/Profile.vue
+++ basics/src/views/Profile.vue
@@ -3,11 +3,31 @@
     <section class="user-banner-profile">
       <div class="user-profile"></div>
       <p id="logo-for-feed">
-        <router-link to="/"><img src="tv.jpg" class="logo" ></router-link>
+        <router-link to="/"><img src="tv.jpg" class="logo"/></router-link>
       </p>
       <p id="banner-profile-empty-space"></p>
-      <p id="settings">Settings</p>
-      
+      <div id="settings" class="w3-container">
+        <button
+          onclick="document.getElementById('id01').style.display='block'"
+          class="w3-button w3-black"
+          id="settings-button"
+        >
+          <b-icon icon="gear"></b-icon>
+        </button>
+
+        <div id="id01" class="w3-modal">
+          <div class="w3-modal-content">
+            <div class="w3-container">
+              <span
+                onclick="document.getElementById('id01').style.display='none'"
+                class="w3-button w3-display-topright"
+                >&times;</span
+              >
+              <p>Some text. Some text. Some text.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
 
     <section class="info-about-user">
@@ -29,45 +49,56 @@
       <p id="user-banner-button">Liked Posts</p>
     </section>
 
-    <section class = "user-page-post"> 
-
-  <div class= "user-box-1">
-  <div class="w3-container">
-  <button onclick="document.getElementById('id01').style.display='block'" class="w3-button w3-black">
-    <div class= "user-image-1"> image </div>
-    <h2 class = "user-likes">           
-      <b-icon variant="danger" icon="heart"></b-icon> 1 like
-      </h2>
-  </button>
-  </div>
-
-  <div id="id01" class="w3-modal">
-    <div class="w3-modal-content">
-      <div class="w3-container">
-        <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-display-topright">&times;</span>
-      <div class="modal-header"> </div>
-      <div class="feed-post">
-        <div class="picture">
-          <b-dropdown variant="none" class="report-button" size="lg" no-caret>
-            <template #button-content>
-              <span>...</span>
-            </template>
-            <b-dropdown-item href="#">Report</b-dropdown-item>
-          </b-dropdown>
-
-          <div class="likes">
-            <b-icon variant="danger" icon="heart"></b-icon> 1 like
-          </div>
+    <section class="user-page-post">
+      <div class="user-box-1">
+        <div class="w3-container">
+          <button
+            onclick="document.getElementById('id01').style.display='block'"
+            class="w3-button w3-black"
+          >
+            <div class="user-image-1">image</div>
+            <h2 class="user-likes">
+              <b-icon variant="danger" icon="heart"></b-icon> 1 like
+            </h2>
+          </button>
         </div>
 
-        <div class="description-comment">
-          <div class="description">
-            <router-link to="/ProfileOther" class="username">
-              Name
-            </router-link>
-            <p class="caption">caption</p>
-          </div>
-          <!-- <div class="comment-section">
+        <div id="id01" class="w3-modal">
+          <div class="w3-modal-content">
+            <div class="w3-container">
+              <span
+                onclick="document.getElementById('id01').style.display='none'"
+                class="w3-button w3-display-topright"
+                >&times;</span
+              >
+              <div class="modal-header"></div>
+              <div class="feed-post">
+                <div class="picture">
+                  <b-dropdown
+                    variant="none"
+                    class="report-button"
+                    size="lg"
+                    no-caret
+                  >
+                    <template #button-content>
+                      <span>...</span>
+                    </template>
+                    <b-dropdown-item href="#">Report</b-dropdown-item>
+                  </b-dropdown>
+
+                  <div class="likes">
+                    <b-icon variant="danger" icon="heart"></b-icon> 1 like
+                  </div>
+                </div>
+
+                <div class="description-comment">
+                  <div class="description">
+                    <router-link to="/ProfileOther" class="username">
+                      Name
+                    </router-link>
+                    <p class="caption">caption</p>
+                  </div>
+                  <!-- <div class="comment-section">
           <router-link
             to="/ProfileOther"
             id="comment-username"
@@ -77,52 +108,62 @@
           </router-link>
           <p class="comment">comment</p>
         </div> -->
-        </div>
-      </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
-  <div class= "user-box-1">
-  <div class="w3-container">
-  <button onclick="document.getElementById('id01').style.display='block'" class="w3-button w3-black">
-    <div class= "user-image-1"> image </div>
-    <h2 class = "user-likes">           
-      <b-icon variant="danger" icon="heart"></b-icon> 1 like
-      </h2>
-  </button>
-  </div>
-
-  <div id="id01" class="w3-modal">
-    <div class="w3-modal-content">
-      <div class="w3-container">
-        <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-display-topright">&times;</span>
-      <div class="modal-header"> </div>
-      <div class="feed-post">
-        <div class="picture">
-          <b-dropdown variant="none" class="report-button" size="lg" no-caret>
-            <template #button-content>
-              <span>...</span>
-            </template>
-            <b-dropdown-item href="#">Report</b-dropdown-item>
-          </b-dropdown>
-
-          <div class="likes">
-            <b-icon variant="danger" icon="heart"></b-icon> 1 like
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
 
-        <div class="description-comment">
-          <div class="description">
-            <router-link to="/ProfileOther" class="username">
-              Name
-            </router-link>
-            <p class="caption">caption</p>
-          </div>
-          <!-- <div class="comment-section">
+      <div class="user-box-1">
+        <div class="w3-container">
+          <button
+            onclick="document.getElementById('id01').style.display='block'"
+            class="w3-button w3-black"
+          >
+            <div class="user-image-1">image</div>
+            <h2 class="user-likes">
+              <b-icon variant="danger" icon="heart"></b-icon> 1 like
+            </h2>
+          </button>
+        </div>
+
+        <div id="id01" class="w3-modal">
+          <div class="w3-modal-content">
+            <div class="w3-container">
+              <span
+                onclick="document.getElementById('id01').style.display='none'"
+                class="w3-button w3-display-topright"
+                >&times;</span
+              >
+              <div class="modal-header"></div>
+              <div class="feed-post">
+                <div class="picture">
+                  <b-dropdown
+                    variant="none"
+                    class="report-button"
+                    size="lg"
+                    no-caret
+                  >
+                    <template #button-content>
+                      <span>...</span>
+                    </template>
+                    <b-dropdown-item href="#">Report</b-dropdown-item>
+                  </b-dropdown>
+
+                  <div class="likes">
+                    <b-icon variant="danger" icon="heart"></b-icon> 1 like
+                  </div>
+                </div>
+
+                <div class="description-comment">
+                  <div class="description">
+                    <router-link to="/ProfileOther" class="username">
+                      Name
+                    </router-link>
+                    <p class="caption">caption</p>
+                  </div>
+                  <!-- <div class="comment-section">
           <router-link
             to="/ProfileOther"
             id="comment-username"
@@ -132,54 +173,62 @@
           </router-link>
           <p class="comment">comment</p>
         </div> -->
-        </div>
-      </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
-
-
-  <div class= "user-box-1">
-  <div class="w3-container">
-  <button onclick="document.getElementById('id01').style.display='block'" class="w3-button w3-black">
-    <div class= "user-image-1"> image </div>
-    <h2 class = "user-likes">           
-      <b-icon variant="danger" icon="heart"></b-icon> 1 like
-      </h2>
-  </button>
-  </div>
-
-  <div id="id01" class="w3-modal">
-    <div class="w3-modal-content">
-      <div class="w3-container">
-        <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-display-topright">&times;</span>
-      <div class="modal-header"> </div>
-      <div class="feed-post">
-        <div class="picture">
-          <b-dropdown variant="none" class="report-button" size="lg" no-caret>
-            <template #button-content>
-              <span>...</span>
-            </template>
-            <b-dropdown-item href="#">Report</b-dropdown-item>
-          </b-dropdown>
-
-          <div class="likes">
-            <b-icon variant="danger" icon="heart"></b-icon> 1 like
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
 
-        <div class="description-comment">
-          <div class="description">
-            <router-link to="/ProfileOther" class="username">
-              Name
-            </router-link>
-            <p class="caption">caption</p>
-          </div>
-          <!-- <div class="comment-section">
+      <div class="user-box-1">
+        <div class="w3-container">
+          <button
+            onclick="document.getElementById('id01').style.display='block'"
+            class="w3-button w3-black"
+          >
+            <div class="user-image-1">image</div>
+            <h2 class="user-likes">
+              <b-icon variant="danger" icon="heart"></b-icon> 1 like
+            </h2>
+          </button>
+        </div>
+
+        <div id="id01" class="w3-modal">
+          <div class="w3-modal-content">
+            <div class="w3-container">
+              <span
+                onclick="document.getElementById('id01').style.display='none'"
+                class="w3-button w3-display-topright"
+                >&times;</span
+              >
+              <div class="modal-header"></div>
+              <div class="feed-post">
+                <div class="picture">
+                  <b-dropdown
+                    variant="none"
+                    class="report-button"
+                    size="lg"
+                    no-caret
+                  >
+                    <template #button-content>
+                      <span>...</span>
+                    </template>
+                    <b-dropdown-item href="#">Report</b-dropdown-item>
+                  </b-dropdown>
+
+                  <div class="likes">
+                    <b-icon variant="danger" icon="heart"></b-icon> 1 like
+                  </div>
+                </div>
+
+                <div class="description-comment">
+                  <div class="description">
+                    <router-link to="/ProfileOther" class="username">
+                      Name
+                    </router-link>
+                    <p class="caption">caption</p>
+                  </div>
+                  <!-- <div class="comment-section">
           <router-link
             to="/ProfileOther"
             id="comment-username"
@@ -189,29 +238,20 @@
           </router-link>
           <p class="comment">comment</p>
         </div> -->
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
     </section>
-
-
   </div>
 </template>
-
 
 <script>
 export default {};
 </script>
 
 <style lang="scss" scoped>
-
 @import "@/styles/Profile.scss";
-
 </style>
-
