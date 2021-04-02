c810171544ea92076c1fbba6593610c61ab334a1
bickyy <vicky.zhou21@sitechhs.com>
2021-04-02 18:05:31 -0400

    got settings change pfp button to work

--- basics/src/App.vue
+++ basics/src/App.vue
@@ -1,44 +1,33 @@
 <template>
   <div id="app">
     <top-header></top-header>
-      <div id="nav">
-        <router-link to="/" >Logo</router-link> |
-        <router-link to="/login" v-if="!loggedIn">Login </router-link> |
-        <router-link to="/secret"> Secret </router-link> |
-        <router-link to="/profile" v-if="loggedIn"> Profile </router-link> |
-       <router-link to="/profileOther"> Profile2 </router-link>
-    
-      </div>
+    <div id="nav"></div>
     <router-view />
   </div>
 </template>
 
-<script> 
-
+<script>
 import firebase from "firebase/app";
-import TopHeader from "./components/Top-Header"
+import TopHeader from "./components/Top-Header";
 export default {
-  components: {'top-header': TopHeader},
-  mounted(){
+  components: { "top-header": TopHeader },
+  mounted() {
     this.getUserStatus();
   },
-  methods:{
-    getUserStatus(){
-      this.loggedIn = firebase.auth().currentUser
+  methods: {
+    getUserStatus() {
+      this.loggedIn = firebase.auth().currentUser;
       console.log(this.loggedIn);
-    }
+    },
   },
   data() {
-    
     return {
-      loggedIn: false
+      loggedIn: false,
     };
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss">
-
 @import "@/styles/App.scss";
-
 </style>
