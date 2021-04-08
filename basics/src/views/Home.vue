70a50335d701be3a23e6a11481e8b1ba5eb50723
colleenww <colleenwhwang@gmail.com>
2021-04-08 12:26:59 -0400

    add back image preview

--- basics/src/views/Home.vue
+++ basics/src/views/Home.vue
@@ -9,27 +9,30 @@
         <v-select id="mySelect" :options="options"></v-select>
       </div>
       <!-- <v-select id="select-tag-container" :options="options"></v-select> -->
-      <button
-        onclick="document.getElementById('id01').style.display='block'"
-        class="w3-button w3-black"
-      >
+      <button onclick="document.getElementById('id01').style.display='block'"
+      class="w3-button w3-black">
         +
       </button>
 
       <div id="id01" class="w3-modal">
         <div class="w3-modal-content">
           <div class="w3-container">
-            <span
-              onclick="document.getElementById('id01').style.display='none'"
-              class="w3-button w3-display-topright"
-              >&times;</span
-            >
+            <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-display-topright">
+              &times;
+            </span>
             <div class="modal-body">
               <div class="modal-header"></div>
               <div class="blog-picture">
-                <b-form-file
-                  accept="image/jpeg, image/png, image/gif"
-                ></b-form-file>
+                <b-container class="mt-3" fluid>
+                  <b-form @submit.stop.prevent="onSubmit">
+                    <div class="d-flex mb-3">
+                      <b-form-file v-model="image" placeholder="Choose an image" class="w-auto flex-grow-1"></b-form-file>
+                      <b-button v-if="hasImage" variant="danger" class="ml-3" @click="clearImage">Clear image</b-button>
+                    </div>
+                    <b-img v-if="hasImage" :src="imageSrc" class="mb-3" fluid block rounded></b-img>
+                    <b-button :disabled="!hasImage" variant="primary" type="submit">Upload image</b-button>
+                  </b-form>
+                </b-container>
               </div>
               <div class="blog-comment">
                 <textarea placeholder="Caption" class="blog-caption"></textarea>
@@ -103,12 +106,18 @@
 <script>
 export default {
   computed: {
+    hasImage() {
+      return !!this.image;
+    },
     captionState() {
       return this.caption.length < 150 ? true : false;
     },
   },
   data() {
     return {
+      image: null,
+      imageSrc: null,
+
       caption: "",
       blog: {
         tag: "",
@@ -132,8 +141,38 @@ export default {
       ],
     };
   },
+watch: {
+    image(newValue, oldValue) {
+      if (newValue !== oldValue) {
+        if (newValue) {
+          base64Encode(newValue)
+            .then(value => {
+              this.imageSrc = value;
+            })
+            .catch(() => {
+              this.imageSrc = null;
+            });
+        } else {
+          this.imageSrc = null;
+        }
+      }
+    }
+  },
+  methods: {
+    clearImage() {
+      this.image = null;
+    },
+    onSubmit() {
+      if (!this.image) {
+        alert("Please select an image.");
+        return;
+      }
+      alert("Form submitted!");
+    }
+  }
 };
 
+//banner scroll effect
 let scrollPos = 0;
 
 window.addEventListener("scroll", function() {
@@ -148,6 +187,15 @@ window.addEventListener("scroll", function() {
   }
   scrollPos = document.body.getBoundingClientRect().top;
 });
+
+//upload image preview
+const base64Encode = data =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(data);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
 </script>
 
 <style lang="scss">
